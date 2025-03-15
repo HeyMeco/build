4f66f0b20558c5ed39437676ddd03df97fefafed
Mecid <mecid@mecomediagroup.de>
2025-03-15 09:25:16 +0100

    Genio-BSP: First add-apt-repository hack

--- extensions/genio-1200-bsp.sh
+++ extensions/genio-1200-bsp.sh
@@ -23,10 +23,10 @@ function post_install_kernel_debs__genio() {
 	# Only apply for jammy
 	[[ "${RELEASE}" != "jammy" ]] && return 0
 
-	do_with_retries 3 chroot_sdcard echo Why does the first add-apt-repository not show up in the apt sources list?
-
 	# Add Canonical HW enablement Repository
 	display_alert "Adding Canonical HW enablement Repository" "${EXTENSION}" "info"
+	do_with_retries 1 chroot_sdcard add-apt-repository -s "deb http://oem.archive.canonical.com/ jammy-baoshan public" --yes --no-update
+	#Doing it twice fixes the issue with the first add-apt-repository not showing up in the apt sources list: WHY?
 	do_with_retries 3 chroot_sdcard add-apt-repository -s "deb http://oem.archive.canonical.com/ jammy-baoshan public" --yes --no-update
 	# Add Canonical HW enablement Repository key
 	do_with_retries 3 chroot_sdcard apt-key adv --recv-keys --keyserver keyserver.ubuntu.com F9FDA6BED73CDC22
