7029ce435535bc34b6d39c0beddab78d730c6d2f
Mecid Urganci <mecid@mecomediagroup.de>
2025-03-19 17:31:06 +0100

    Genio: Add DDK r48p0 patch

---
+++ extensions/genio-1200-bsp.sh
@@ -0,0 +1,64 @@
+#
+# SPDX-License-Identifier: GPL-2.0
+# Armbian build framework extension
+#
+# Adds Mediatek Genio BSP support with Mali GPU drivers
+#
+
+function extension_prepare_config__genio() {
+	# Only apply for jammy
+	[[ "${RELEASE}" != "jammy" ]] && return 0
+
+	# Deny on minimal CLI images
+	if [[ "${BUILD_MINIMAL}" == "yes" ]]; then
+		display_alert "Extension: ${EXTENSION}" "skip installation in minimal images" "warn"
+		return 0
+	fi
+
+	# Add image suffix to indicate Genio BSP support
+	EXTRA_IMAGE_SUFFIXES+=("-genio-bsp")
+}
+
+function post_install_kernel_debs__genio() {
+	# Only apply for jammy
+	[[ "${RELEASE}" != "jammy" ]] && return 0
+
+	# Add Canonical HW enablement Repository
+	#display_alert "Adding Canonical HW enablement Repository" "${EXTENSION}" "info"
+	#do_with_retries 1 chroot_sdcard add-apt-repository -s "deb http://oem.archive.canonical.com/ jammy-baoshan public" --yes --no-update
+	#Doing it twice fixes the issue with the first add-apt-repository not showing up in the apt sources list: WHY?
+	#do_with_retries 3 chroot_sdcard add-apt-repository -s "deb http://oem.archive.canonical.com/ jammy-baoshan public" --yes --no-update
+	# Add Canonical HW enablement Repository key
+	#do_with_retries 3 chroot_sdcard apt-key adv --recv-keys --keyserver keyserver.ubuntu.com F9FDA6BED73CDC22
+	#Pin libmali-mtk-8195
+	#display_alert "Pinning Canonical HW enablement Repository" "${EXTENSION}" "info"
+	#cat <<- EOF > "${SDCARD}"/etc/apt/preferences.d/oem-archive-jammy-baoshan-pin
+	#	Package: libmali-mtk-8195
+	#	Pin: release o=oem.archive.canonical.com
+	#	Pin-Priority: 1001
+	#EOF
+
+	# Add Mediatek Genio PPA
+	display_alert "Adding Mediatek Genio Public PPA" "${EXTENSION}" "info"
+	do_with_retries 3 chroot_sdcard add-apt-repository ppa:mediatek-genio/genio-public --yes --no-update
+	# Pin Mediatek Genio PPA
+	display_alert "Pinning Mediatek Genio Public PPA" "${EXTENSION}" "info"
+	cat <<- EOF > "${SDCARD}"/etc/apt/preferences.d/mediatek-genio-public-pin
+		Package: *
+		Pin: release o=LP-PPA-mediatek-genio-genio-public
+		Pin-Priority: 1001
+	EOF
+
+	display_alert "Updating sources list, after adding all PPAs" "${EXTENSION}" "info"
+	do_with_retries 3 chroot_sdcard_apt_get_update
+
+	display_alert "Installing Genio BSP packages" "${EXTENSION}" "info"
+	#do_with_retries 3 chroot_sdcard_apt_get_install "libmali-mtk-8195"
+	do_with_retries 3 chroot_sdcard_apt_get_install "mediatek-vpud-genio1200"
+	do_with_retries 3 chroot_sdcard_apt_get_install "mediatek-apusys-firmware-genio1200"
+
+	display_alert "Upgrading all packages" "${EXTENSION}" "info"
+	do_with_retries 3 chroot_sdcard_apt_get -o Dpkg::Options::="--force-confold" --allow-downgrades dist-upgrade
+
+	return 0
+}
